11e1a97fb976b155b86d16bd7320d7d516069b5f
Andrew <andrey.shemiakov@gmail.com>
2016-10-02 14:34:21 +0300

    about

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var stripCssComments = require('gulp-strip-css-comments');
 var cssbeautify = require('gulp-cssbeautify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-    gulp.src('./sass/**/*.scss')
+    gulp.src('./sass/**/main.scss')
+    // gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(stripCssComments())
         .pipe(cssbeautify())
+        .pipe(sourcemaps.write('/'))
         .pipe(gulp.dest('./css'));
 });
 
