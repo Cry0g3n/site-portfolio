ef29cd29ba6711a0d4979e376251ed21dbc27783
Andrew <andrey.shemiakov@gmail.com>
2016-10-09 01:16:48 +0300

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,17 @@ var sass = require('gulp-sass');
 var stripCssComments = require('gulp-strip-css-comments');
 var cssbeautify = require('gulp-cssbeautify');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
     gulp.src('./sass/**/main.scss')
-    // gulp.src('./sass/**/*.scss')
         .pipe(sourcemaps.init('/'))
         .pipe(sass().on('error', sass.logError))
         .pipe(stripCssComments())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'Firefox ESR'],
+            cascade: false
+        }))
         .pipe(cssbeautify())
         .pipe(sourcemaps.write('/'))
         .pipe(gulp.dest('./css'));
